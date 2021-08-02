a0d8b15a15d1a8c64589a47859be92fbee3c25d7
Clifton King <cliftonk@gmail.com>
2021-08-01 21:00:30 -0500

    add svg logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,25 @@
-import { Box, Grid, Text, VStack } from '@chakra-ui/react';
+import { Box, Grid, Text, useColorModeValue, VStack } from '@chakra-ui/react';
 import { FunctionComponent } from 'react';
 import { SocialLinks } from 'components/social-links';
+import { Logo } from 'components/Logo';
 import { Layout } from '../components/Layout';
 
 const IndexPage: FunctionComponent = () => (
   <Layout title="atxdao">
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3} mt="5vh">
-        <VStack spacing={[6, 8]}>
+        <VStack spacing={[4, 4, 8]}>
           <Text fontSize={['3.2rem', '4rem', '5rem']} lineHeight={1}>
             atxdao
           </Text>
+          <Logo
+            boxSize={['256px', '256px', '384px']}
+            fill={useColorModeValue('gray.800', 'gray.100')}
+          />
           <SocialLinks
-            fontSize="4xl"
-            spacing={4}
+            fontSize={['2rem', '2rem', '3rem']}
+            color={useColorModeValue('gray.800', 'gray.100')}
+            spacing={[2, 2, 4]}
             socialLinks={[
               {
                 social: 'discord',
