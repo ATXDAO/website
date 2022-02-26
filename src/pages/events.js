7290aec22ac80a39d5435e44cac970bf38a78008
Benjamin00 <benjaminhill00@gmail.com>
2022-02-26 16:35:10 -0600

    Working reads from the API to populate the page, still need the link to work, and the image as well

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,8 +3,9 @@ import Event from '../components/events/Event';
 import Layout from '../components/layout/Layout';
 import { eventData } from '../util/mockData';
 import { Box, Divider, Link } from '@chakra-ui/react';
+import eventbrite from 'eventbrite';
 
-function Events() {
+function Events({ events }) {
   const contentPaddingX = ['1rem', '2rem', '3rem', '10rem'];
   const contentPaddingY = '3rem';
 
@@ -12,7 +13,7 @@ function Events() {
     <Layout>
       <Connect />
       <Box paddingX={contentPaddingX} paddingTop={contentPaddingY}>
-        {eventData.map((obj, i) => (
+        {events.events.map((obj, i) => (
           <>
             {i !== 0 && (
               <Divider
@@ -24,12 +25,27 @@ function Events() {
             <Box mb={['2rem', '3rem', '5rem']}>
               <Link href="#" target="_blank">
                 <Event
-                  title={obj.title}
-                  date={obj.date}
-                  startTime={obj.startTime}
-                  endTime={obj.endTime}
-                  description={obj.desc}
-                  img={obj.img}
+                  title={obj.name.text}
+                  date={new Date(obj.start.local).toLocaleString('en-US', {
+                    weekday: 'long',
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                  })}
+                  startTime={new Date(obj.start.local).toLocaleTimeString(
+                    'en-US',
+                    {
+                      hour: 'numeric',
+                      minute: 'numeric',
+                      hour12: true,
+                    }
+                  )}
+                  endTime={new Date(obj.end.local).toLocaleTimeString('en-US', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    hour12: true,
+                  })}
+                  description={obj.summary}
                 />
               </Link>
             </Box>
@@ -38,6 +54,35 @@ function Events() {
       </Box>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const sdk = eventbrite({ token: 'KMNE2CUAUC2S5453DFYX' });
+  const ORG_ID = '514468661905';
+  let events = null;
+  await sdk
+    .request(`/organizations/${ORG_ID}/events/`)
+    .then((response) => {
+      console.log('Response:', response);
+      events = response;
+    })
+    .catch((errInfo) => {
+      console.error(errInfo.response['error_description']);
+
+      // `ARGUMENT_ERROR` errors are parsed into `parsedError` property
+      const parsedError = errInfo.parsedError;
+
+      // equivalent to errorInfo.response.error
+      console.log(parsedError.error);
+
+      // equivalent to errorInfo.response['error_description']
+      console.log(parsedError.description);
+    });
+  if (!events) {
+    console.log('Didnt get events');
+    return { notFound: true };
+  }
+  return { props: { events } };
 }
 
 export default Events;
